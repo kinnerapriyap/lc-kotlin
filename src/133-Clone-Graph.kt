0765dfb726d32afef052df722475ddef919bac8c
vhontar <vladislav.hontar@gmail.com>
2024-08-07 09:08:41 +0200

    finished clone graph

--- src/133-Clone-Graph.kt
+++ src/133-Clone-Graph.kt
@@ -1,5 +1,14 @@
-val visited = hashMapOf<Int, Node>()
+// https://leetcode.com/problems/clone-graph
 
-fun cloneGraph(node: Node?): Node? {
-   node ?: return null
+import java.util.HashMap
+
+fun cloneGraph(node: Node?, visited: HashMap<Int, Node> = hashMapOf()): Node? {
+   if (node == null) return null
+   if (visited.contains(node.`val`)) return visited[node.`val`]
+   val newNode = Node(node.`val`)
+   visited[newNode.`val`] = newNode
+   node.neighbors.forEach { neighbor ->
+      newNode.neighbors.add(cloneGraph(neighbor, visited))
+   }
+   return newNode
 }

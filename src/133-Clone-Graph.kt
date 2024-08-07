138f36ba32c08ace4f4c0b3aa715a03634283d69
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-08-07 08:56:55 +0200

    Add init for clone graph

---
+++ src/133-Clone-Graph.kt
@@ -0,0 +1,5 @@
+val visited = hashMapOf<Int, Node>()
+
+fun cloneGraph(node: Node?): Node? {
+   node ?: return null
+}
--- src/Helper.kt
+++ src/Helper.kt
@@ -4,3 +4,7 @@ class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
 }
+
+class Node(var `val`: Int) {
+    var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+}
